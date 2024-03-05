b474e9773d6ac6199bd0b516dd259b3badeb914e
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-03-05 18:51:05 +0000

    add functionality of the selected and unselected products

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, ScrollView } from 'react-native'
 
 import { style } from './styles'
 import Ingredient from '@/components/Ingredient'
 
 const Index = () => {
+  const [selected, setSelected] = useState<string[]>([])
+
+  const handleToggleSelected = (value: string) => {
+    if(selected.includes(value)){
+      return setSelected((state) => state.filter((item) => item !== value))
+    }
+
+    setSelected((state) => [...state, value])
+    console.log(selected)
+  }
+
   return (
     <View style={style.container}>
         <Text style={style.title}>
@@ -14,10 +25,11 @@ const Index = () => {
 
         <Text style={style.message}>Discover recipes based on the products you chose</Text>
 
-        <ScrollView horizontal contentContainerStyle={style.ingredients}>
-          <Ingredient/>
-          <Ingredient/>
-          <Ingredient/>
+        <ScrollView  contentContainerStyle={style.ingredients} showsVerticalScrollIndicator={false}>
+          {Array.from({ length: 100 }).map((item, index) => (
+            <Ingredient key={index} name='apple' image='' selected={selected.includes(String(index))} onPress={() => handleToggleSelected(String(index))}/>
+          ))}
+          
         </ScrollView>
     </View>
   )

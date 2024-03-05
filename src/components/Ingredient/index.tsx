b39ad1273479db93ddc1ca06286e74010c87b1ef
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-03-05 18:51:45 +0000

    add props to the Ingredient Component

--- src/components/Ingredient/index.tsx
+++ src/components/Ingredient/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react' 
-import { Pressable, Text, Image } from 'react-native'
+import { Pressable, PressableProps, Text, Image } from 'react-native'
 import { styles } from './styles'
 
-export const Ingredient = () =>  {
+export type IngredientProps = {
+  name: string
+  image: string
+  selected?: boolean
+}
+
+export const Ingredient = ({ name, image, selected = false, ...rest }: IngredientProps & PressableProps) =>  {
   return (
-    <Pressable style={styles.container}>
+    <Pressable style={[styles.container, selected && styles.selected]} {...rest}>
         <Image source={require('@/assets/apple.png')} style={styles.image}/>
 
         <Text style={styles.title}>

1260661ebb9166b60f563393f5c7d4620450fb58
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-03-05 16:37:19 +0000

    start the project

---
+++ src/app/index/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Index = () => {
+  return (
+    <View>
+        <Text> React Native</Text>
+    </View>
+  )
+}
+
+export default Index

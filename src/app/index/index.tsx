7e8fade7bd4a2fed98328c249ee29127efceb377
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-03-07 14:07:54 +0000

    calls Selected Component

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, Alert } from 'react-native'
 
 import { style } from './styles'
 import Ingredient from '@/components/Ingredient'
+import { Selected } from '@/components/Selected'
 
 const Index = () => {
   const [selected, setSelected] = useState<string[]>([])
@@ -13,7 +14,13 @@ const Index = () => {
     }
 
     setSelected((state) => [...state, value])
-    console.log(selected)
+  }
+
+  const handleClearSelected = () => {
+    Alert.alert('Clear', 'Do you want to clear everything?',[
+      { text: 'No', style: 'cancel'},
+      { text: 'Yes', onPress: () => setSelected([])}
+    ])
   }
 
   return (
@@ -29,8 +36,8 @@ const Index = () => {
           {Array.from({ length: 100 }).map((item, index) => (
             <Ingredient key={index} name='apple' image='' selected={selected.includes(String(index))} onPress={() => handleToggleSelected(String(index))}/>
           ))}
-          
         </ScrollView>
+        <Selected quantity={selected.length} onClear={handleClearSelected} onSearch={() => {}}/>
     </View>
   )
 }

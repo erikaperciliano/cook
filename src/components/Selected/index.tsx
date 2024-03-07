64e819b91285b5a1f19891796ed8b458210f47d7
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-03-07 14:12:54 +0000

    add animation

--- src/components/Selected/index.tsx
+++ src/components/Selected/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import Animated from 'react-native-reanimated';
+import Animated, { SlideInDown, BounceOutDown } from 'react-native-reanimated';
 import { MaterialIcons } from '@expo/vector-icons';
 
 import { styles } from './styles';
@@ -14,7 +14,7 @@ type Props = {
 
 export const Selected = ({ quantity, onClear, onSearch }: Props) => {
   return (
-    <Animated.View style={styles.container}>
+    <Animated.View style={styles.container} entering={SlideInDown} exiting={BounceOutDown.duration(500)}>
         <View style={styles.header}>
             <Text style={styles.label}>{quantity} ingredients selected</Text>
             <MaterialIcons name='close' size={24} onPress={onClear} color={theme.colors.gray_400}/>
